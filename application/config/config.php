5321b0cf7b38b30306f7a49798f8c0346faccbc0
SammWell668899 <223240041@student.unaki.ac.id>
2026-08-29 19:10:51 +0700

    update base URL in config to point to the live site

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/rosok_app_1/';
+$config['base_url'] = 'https://sammwell668899.github.io/inventarisBarangCI3/';
 
 /*
 |--------------------------------------------------------------------------
